7da9eee4f0f7aec19db2cd50961326bc46fe9eb0
sathyaravi <sathyaravichandran92@gmail.com>
2025-10-03 14:20:48 -0700

    Add files via upload
    Week1 - Day1 -Homework

---
+++ week1/day1/homework/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package week1.day1.homework;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+
+		String name = "John Doe";
+		int age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		double pincode = 56001;
+		int luckyNumber = 7;
+		
+		System.out.println("Name of the employee:"+name);
+		System.out.println("Age:"+age);
+		System.out.println("Salary:"+salary);
+		System.out.println("Gender:"+gender);
+		System.out.println("Term Acceptance:"+acceptTerms);
+		System.out.println("Rating:"+rating);
+		System.out.println("Contact Number:" +mobileNumber);
+		System.out.println("Pincode:"+pincode);
+		System.out.println("Lucky Number:"+luckyNumber);
+		
+		
+
+	}
+
+}
