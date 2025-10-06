df20b8764fb7fca720ef19862f9ceb2401ed3600
sathyaravi <sathyaravichandran92@gmail.com>
2025-10-06 16:58:18 -0700

    Week2 Day 1 Assignment

---
+++ FindOddNumbersUsingWhile.java
@@ -0,0 +1,19 @@
+package week2day1;
+
+public class FindOddNumbersUsingWhile {
+
+	public static void main(String[] args) {
+		
+		int maxRange = 10,i=0;
+		while (i<=maxRange) {
+			if (i%2 == 1) {
+				System.out.println(i);
+			}
+			i++;
+			
+		}
+		
+
+	}
+
+}
